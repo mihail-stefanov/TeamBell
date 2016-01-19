2a21c31a14f9ad47f680326e675f900676d1d611
dmaslev <detelinmaslev@gmail.com>
2016-01-19 05:20:01 +0200

    Figirec constructor done. Rotate method done.

---
+++ Resources/Scripts/figures.js
@@ -0,0 +1,58 @@
+var figures = [
+    // L-figure
+    [[1, 0],
+        [1, 0],
+        [1, 1]],
+    // J-figure
+    [[0, 1],
+        [0, 1],
+        [1, 1]],
+    // O-figure
+    [[1, 1],
+        [1, 1]],
+    // I-figure
+    [[1],
+        [1],
+        [1],
+        [1]],
+    // T-figure
+    [[1, 0],
+        [1, 1],
+        [1, 0]],
+    // Z-figure
+    [[1, 1, 0],
+        [0, 1, 1]],
+    // S-figure
+    [[0, 1, 1],
+        [1, 1, 0]]
+];
+
+function Figure(index) {
+    this.color = 0;
+    this.y = 0;
+    this.x = 4;
+    this.matrix = figures[index];
+}
+
+function rotateFigure(figure) {
+    var temp = [];
+    for (var i = 0; i < figure.matrix[0].length; i++) {
+        temp[i] = [];
+        for (var j = 0; j < figure.matrix.length; j++) {
+            temp[i][figure.matrix.length - j - 1] = figure.matrix[j][i];
+        }
+    }
+
+    return temp;
+}
+
+
+function generateFigure() {
+    var piece;
+    var figureTypeIndex = Math.floor(Math.random() * 7);
+    var pieceColor = Math.floor(Math.random());
+
+    piece = new Figure(figureTypeIndex);
+    piece.color = pieceColor;
+    return piece;
+}
