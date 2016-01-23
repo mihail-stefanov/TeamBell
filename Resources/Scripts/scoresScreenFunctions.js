ced1b53c599ba5648f2d7d561ffa686857d577e4
Mihail Stefanov <mihail-stefanov@users.noreply.github.com>
2016-01-23 21:05:15 +0200

    Added scores.json file containing sample scores that can be used as a comparison, when a player finishes a game. Created an XMLHttpRequest to extract scores from the JSON file, which are now drawn on the High Scores screen.

--- Resources/Scripts/scoresScreenFunctions.js
+++ Resources/Scripts/scoresScreenFunctions.js
@@ -2,6 +2,21 @@
 
 // ==================== DEFINITIONS OF THINGS TO EXIST ON THE SCORES SCREEN ====================
 
+// Extracting the scores from a JSON file via AJAX
+var scores;
+
+var dataRequest = new XMLHttpRequest();
+
+dataRequest.onreadystatechange = function() {
+    if(dataRequest.readyState == 4 && dataRequest.status === 200) {
+        scores = JSON.parse(dataRequest.responseText);
+    }
+};
+
+// Performed only once when the game is loaded so that a new score can be added by the current player
+dataRequest.open("GET", "Resources/Data/scores.json", false); // not asynchronous
+dataRequest.send(null); // Sending the request without any parameters
+
 function initializeScoresScreenElements() {
     buttons = new Array();
     buttons.push(new Button(740,10,50,50,"gray", buttonName.exit));
@@ -31,6 +46,35 @@ function drawScoresScreenText() {
     
 }
 
+function drawScores() { 
+    context.font = 'bold 25px Consolas';
+    
+    context.fillStyle = "rgb(224, 224, 224)";
+    context.fillRect(75, 83, 615, 40);
+    context.fillStyle = "black";
+    context.fillText("No.", 100, 110);
+    context.fillText("Name", 200, 110);
+    context.fillText("Score", 600, 110);
+    
+    context.font = '25px Consolas';
+    for (var i = 0; i < scores.length; i++) {
+        
+        // Drawing a darker background every even line
+        if (i % 2 == 0) {
+            context.fillStyle = "rgb(154, 154, 154)";
+        } else {
+            context.fillStyle = "rgb(224, 224, 224)";
+        }
+        context.fillRect(75, 83 + (i + 1) * 35, 615, 35);
+        
+        context.fillStyle = "black";
+        var currentPlace = Number(i + 1) + ".";
+        context.fillText(currentPlace, 100, 110 + (i + 1) * 35);
+        context.fillText(scores[i].name, 200, 110 + (i + 1) * 35);
+        context.fillText(scores[i].score, 600, 110 + (i + 1) * 35);
+    }
+}
+
 // ==================== FUNCTION CALLED REPETITIVELY ====================
 
 function drawScoresScreen() {
@@ -40,6 +84,8 @@ function drawScoresScreen() {
         drawScoresScreenButtons();
         
         drawScoresScreenText();
+        
+        drawScores();
 
         redrawingIsNeeded = false;
     }
